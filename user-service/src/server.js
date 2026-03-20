1b71461d6d073fc2a38d315b6e3b5dfb443fb877
Sharix111 <sharshithab2021@gmail.com>
2026-03-20 23:25:03 +0530

    fix(server): standardize server start comments across services

--- user-service/src/server.js
+++ user-service/src/server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
-
+// Start server
 const startServer = async () => {
   try {
     await connectDB();
